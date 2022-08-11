5590acb75ac55d054c392cfdec21e0d7c0083ff2
Salavat Aksarin <theowalcott9@gmail.com>
2022-08-12 02:38:34 +0300

    PP1.1.3
    First leak

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,31 @@
+package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private final static String URL = "jdbc:mysql://localhost:3306/dbtest";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "36350022qQ!";
+
+    public static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection complete successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void connectionClose() {
+        try {
+            connection.close();
+            System.out.println("Connection close successfully");
+        } catch (NullPointerException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
